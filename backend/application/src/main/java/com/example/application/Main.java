d0cebd6a8d4ddb8ad217b053231c175e26191991
monika-sokolowska <monika.sokolowska@student.pk.edu.pl>
2023-04-16 12:18:06 +0200

    folder changes

--- backend/application/src/main/java/com/example/application/Main.java
+++
@@ -1,9 +0,0 @@
-package com.example.application;
-
-import com.example.application.view.ServicePicker;
-
-public class Main {
-    public static void main(String[] args) {
-        ServicePicker.main(args);
-    }
-}
